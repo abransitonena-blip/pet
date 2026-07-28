559d2086b0b021b5e9e0bef6d850fa161916940e
abraham <abransitonena@gmail.com>
2026-07-28 16:43:23 -0600

    fix: import faltante FaPaw, FaCheck, FaUser en ReservationForm

--- src/components/ReservationForm.tsx
+++ src/components/ReservationForm.tsx
@@ -17,7 +17,7 @@ import StepContact from './reservation-steps/StepContact'
 import StepWalker from './reservation-steps/StepWalker'
 import StepConfirm from './reservation-steps/StepConfirm'
 import {
-  FaSpinner, FaArrowRight, FaArrowLeft, FaWhatsapp, FaCheckCircle, FaCalendarAlt, FaClock,
+  FaSpinner, FaArrowRight, FaArrowLeft, FaWhatsapp, FaCheckCircle, FaCalendarAlt, FaClock, FaPaw, FaCheck, FaUser,
 } from 'react-icons/fa'
 
 const STEP_META = [
